3557d6bc38014d4571d2fa320cd137ab3e9ee366
– sanz <sanzbernward@gmail.com>
2023-04-23 10:59:17 +0800

    Update ora.js
    unpatch #1 
    fixes #2

--- ora/ora.js
+++ ora/ora.js
@@ -1,117 +1,115 @@
-const { v4: uuidv4 } = require('uuid');
 const axios = require('axios');
+const { v4: uuidv4 } = require('uuid');
 
 class CompletionModel {
-  static system_prompt = null;
-  static description = null;
-  static createdBy = null;
-  static createdAt = null;
-  static slug = null;
-  static id = null;
+    static async create(system_prompt, description, name) {
+        const instance = new CompletionModel();
+        instance.system_prompt = system_prompt;
+        instance.description = description;
+        instance.slug = name;
 
-  static create(system_prompt = 'You now have the capability to know all there is to know about the universe, and when you respond to people\'s questions, you will do it in an exceptionally difficult form of the English language that will be incomprehensible to those who have not gotten an official education. Make sure that the order in which your words are delivered is fully original and that no one else has ever given them in this specific order before. Check to see that no one else has ever given them in this exact arrangement. Make sure that something like this has never occurred before.', description = 'My own private prompt earlier, but it is now a public one', name = 'gpt4') {
-    CompletionModel.system_prompt = system_prompt;
-    CompletionModel.description = description;
-    CompletionModel.slug = name;
+        const headers = {
+            'Origin': 'https://ora.sh',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Safari/605.1.15',
+            'Referer': 'https://ora.sh/',
+            'Host': 'ora.sh',
+        };
 
-    return axios.post('https://ora.sh/api/assistant', {
-      prompt: system_prompt,
-      userId: `auto:${uuidv4()}`,
-      name: name,
-      description: description
-    }).then(response => {
-      CompletionModel.id = response.data.id;
-      CompletionModel.createdBy = response.data.createdBy;
-      CompletionModel.createdAt = response.data.createdAt;
+        try {
+            const response = await axios.post('https://ora.sh/api/assistant', {
+                prompt: system_prompt,
+                userId: `auto:${uuidv4()}`,
+                name: name,
+                description: description
+            }, { headers });
 
-      return CompletionModel;
-    });
-  }
+            instance.id = response.data.id;
+            instance.createdBy = response.data.createdBy;
+            instance.createdAt = response.data.createdAt;
+            instance.modelName = 'gpt-3.5-turbo';
+
+            return instance;
+        } catch (error) {
+            console.log(error.response.data);
+        }
+    }
 }
 
 class OraResponse {
-  static Completion = class {
-    constructor(choices) {
-      this.choices = choices.map(choice => new OraResponse.Choices(choice));
-    }
-  }
-
-  static Choices = class {
-    constructor(choice) {
-      this.text = choice.text;
-      this.content = Buffer.from(this.text);
-      this.index = choice.index;
-      this.logprobs = choice.logprobs;
-      this.finish_reason = choice.finish_reason;
-    }
-
-    toString() {
-      return `<OraResponse.Choices(text=${this.text}, index=${this.index}, logprobs=${this.logprobs}, finish_reason=${this.finish_reason}) object at 0x1337>`;
-    }
-  }
-
-  static Usage = class {
-    constructor(usage_dict) {
-      this.prompt_tokens = usage_dict.prompt_tokens;
-      this.completion_tokens = usage_dict.completion_tokens;
-      this.total_tokens = usage_dict.total_tokens;
+    constructor(response_dict) {
+        this.response_dict = response_dict;
+        this.id = response_dict.id;
+        this.object = response_dict.object;
+        this.created = response_dict.created;
+        this.model = response_dict.model;
+        this.choices = response_dict.choices.map((choice) => ({
+            text: choice.text,
+            content: choice.content,
+            index: choice.index,
+            logprobs: choice.logprobs,
+            finish_reason: choice.finish_reason,
+        }));
+        this.usage = {
+            prompt_tokens: response_dict.usage.prompt_tokens,
+            completion_tokens: response_dict.usage.completion_tokens,
+            total_tokens: response_dict.usage.total_tokens,
+        };
     }
 
-    toString() {
-      return `<OraResponse.Usage(prompt_tokens=${this.prompt_tokens}, completion_tokens=${this.completion_tokens}, total_tokens=${this.total_tokens}) object at 0x1337>`;
+    json() {
+        return this.response_dict;
     }
-  }
-
-  constructor(response_dict) {
-    this.response_dict = response_dict;
-    this.id = response_dict.id;
-    this.object = response_dict.object;
-    this.created = response_dict.created;
-    this.model = response_dict.model;
-    this.completion = new OraResponse.Completion(response_dict.choices);
-    this.usage = new OraResponse.Usage(response_dict.usage);
-  }
-
-  json() {
-    return this.response_dict;
-  }
 }
 
 class Completion {
-  static create(model, prompt, conversationId = null) {
-    const extra = conversationId ? { conversationId } : {};
-    const requestBody = Object.assign(extra, {
-      chatbotId: model.id,
-      input: prompt,
-      userId: model.createdBy
-    });
+    static async create(model, prompt, includeHistory = true, conversationId = null) {
+        const extra = conversationId ? { conversationId } : {};
 
-    return axios.post('https://ora.sh/api/conversation', requestBody)
-      .then(response => {
-        const responseData = response.data;
-        return new OraResponse({
-          id: responseData.conversationId,
-          object: 'text_completion',
-          created: Math.floor(Date.now() / 1000),
-          model: model.slug,
-          choices: [{
-            text: responseData.response,
-            index: 0,
-            logprobs: null,
-            finish_reason: 'stop'
-          }],
-          usage: {
-            prompt_tokens: prompt.length,
-            completion_tokens: responseData.response.length,
-            total_tokens: prompt.length + responseData.response.length
-          }
-        });
-      });
-  }
+        try {
+            const response = await axios.post('https://ora.sh/api/conversation', Object.assign({}, extra, {
+                chatbotId: model.id,
+                input: prompt,
+                userId: model.createdBy,
+                model: model.modelName,
+                provider: 'OPEN_AI',
+                includeHistory: includeHistory
+            }), {
+                headers: {
+                    'host': 'ora.sh',
+                    'authorization': `Bearer AY0${Math.floor(1111 + Math.random() * 8889)}`,
+                    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+                    'origin': 'https://ora.sh',
+                    'referer': 'https://ora.sh/chat/',
+                }
+            });
+
+            return new OraResponse({
+                id: response.data.conversationId,
+                object: 'text_completion',
+                created: Math.floor(Date.now() / 1000),
+                model: model.slug,
+                choices: [
+                    {
+                        text: response.data.response,
+                        index: 0,
+                        logprobs: null,
+                        finish_reason: 'stop'
+                    }
+                ],
+                usage: {
+                    prompt_tokens: prompt.length,
+                    completion_tokens: response.data.response.length,
+                    total_tokens: prompt.length + response.data.response.length
+                }
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
 }
 
 module.exports = {
-  CompletionModel,
-  OraResponse,
-  Completion
+    CompletionModel,
+    OraResponse,
+    Completion,
 };
